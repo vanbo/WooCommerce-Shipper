aeda3c5ea933b3db1b3c0691a479a2da6f2f83b0
Andy Zhang <andy_zhang@hypnoticzoo.com>
2012-12-06 01:15:46 -0500

    Add package class to handle packing more properly.

---
+++ class-package.php
@@ -0,0 +1,154 @@
+<?php
+/*
+ * HypnoticPackage
+ *
+ * HypnoticPackage contains the shopping cart items. It manage and packing the items
+ * in certain way. 
+ *
+ * @class       HypnoticPackage
+ * @version     1.0
+ * @package     WooCommerce-Shipper/Classes
+ * @author      Andy Zhang
+*/
+
+class HypnoticPackage{
+
+    /**
+    * @var array
+    */
+    var $items = array();
+
+    /**
+    * @var array
+    */
+    var $containers = array();
+
+    /**
+    * @var array
+    */
+    var $packed_containers = array();
+
+    /**
+    * @var array
+    */
+    var $unpackable_items = array();
+
+    /**
+    * @var string
+    */
+    var $weight_unit = 'lbs';
+
+    /**
+    * @var string
+    */
+    var $dimension_unit = 'in';
+
+    /**
+    * The constructor takes cart and containers as parameters
+    */
+    public function __construct( $cart, $containers = array() ){
+        global $woocommerce;
+
+        // Get containers, with their reset dimensions
+        foreach ( $containers as $container ) {
+            $this->reset_position( $container );
+            $this->containers[] = $container;
+        }
+
+        // Put shippable products into package regardless their order or details.
+        foreach ( $cart as $product_id => $product ) {
+            $item = $product['data'];
+            $quantity = $product['quantity'];
+
+            if ( $item->exists() && $quantity > 0 && $item->needs_shipping() ) {
+
+                $this->reset_position( $item );
+
+                for ($i = 0; $i < $quantity; $i++) {
+                    $this->items[] = $item;
+                }
+
+            }
+
+        }
+
+        $this->items = $this->volume_sort( $this->items, 'desc' );
+        $this->containers = $this->volume_sort( $this->containers );
+
+    }
+
+    /**
+    * Packing follows following rules;
+    * 1. Starts from largest item with smallest box
+    * 2. Return packed boxes with total weight, and unpacked items
+    */
+    public function packing () {
+        while( count( $this->items ) ){
+
+            foreach ( $this->items as $id => $item ) {
+
+                if ( $this->unpackable( $item ) ) {
+                    $this->unpackable_items[] = $item;
+                    unset($this->items[$id]);
+                    continue;
+                }
+
+            }
+
+        }
+    }
+
+    /**
+    * Reset the entity to have its dimension in such order:
+    * Height <= Width <= Length
+    */
+    public function reset_position ( &$entity ) {
+        $dimensions = array($entity['height'], $entity['width'], $entity['length']);
+        sort($dimensions);
+        list($entity['height'], $entity['width'], $entity['length']) = $dimensions;
+    }
+
+    /**
+    * Check if an item is unpackable for all containers
+    */
+    public function unpackable ( $item ) {
+        foreach ( $this->containers as $container ) {
+            if ( !$this->can_fit ($item, $container) )
+                return false;
+        }
+        return true;
+    }
+
+    /**
+    * Check if an item can fit into the box
+    */
+    public function can_fit ( $item, $box ) {
+        return $item['width'] <= $box['width'] && $item['length'] <= $box['length'] && $item['height'] <= $box['height'];
+    }
+
+    /**
+    * Get the volume of an item or a box
+    */
+    public function get_volume ( $entity ) {
+        return $entity['height'] * $entity['length'] * $entity['width'];
+    }
+
+    public static function compare_volume ( $alpha, $omega ) {
+        return ( $this->get_volume( $alpha ) > $this->get_volume( $omega ) ) ? 1 : -1;
+    }
+
+    /**
+    * Sort items or boxes on their volume
+    */
+    public function volume_sort ( $entities, $order = 'asc' ) {
+        usort ( $entities, array('HypnoticPackage', 'compare_volume') );
+
+        if ( $order == 'desc' ) {
+            return array_reverse( $entities );
+        }
+
+        return $entities;
+    }
+}
+
+?>
